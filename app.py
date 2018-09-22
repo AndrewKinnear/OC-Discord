497941ab297170bdb3dbe2e9ead3574bcad24f0b
Steven Bucholtz <Stevenabucholtz@gmail.com>
2018-09-22 01:36:41 +0000

    First commit

---
+++ app.py
@@ -0,0 +1,82 @@
+import discord
+import sqlite3 as sql
+from contextlib import contextmanager
+
+TOKEN = "EATING SUGAR? NO PAPA!"
+client = discord.Client()
+
+@contextmanager
+def connection_to_database():
+    try:
+        connection = sql.connect('quiz.db')
+        yield connection
+        connection.close()
+    except sql.Error as e:
+        print("connection_to_database(): connection error: " + e)
+
+
+def dbLookup(query, *args):
+    with connection_to_database() as connection:
+        connection.row_factory = sql.Row
+        c = connection.cursor()
+        c.execute(query, (args))
+        connection.commit()
+        rows = c.fetchall()
+        return rows
+
+
+def dbAlter(query, *args):
+    with connection_to_database() as connection:
+        c = connection.cursor()
+        c.execute(query, (args))
+        connection.commit()
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!add'):
+        answers = []
+
+        await client.send_message(message.channel, "---- Add a question to the database: ----")
+        question = await client.wait_for_message(author=message.author)
+        question = question.content
+
+        await client.send_message(message.channel, "Please add an answer:")
+
+        for i in range(4):
+            msg = await client.wait_for_message(author=message.author)
+            answers.append(msg.content)
+            fmt = '{} left to go...'
+            await client.send_message(message.channel, fmt.format(3 - i))
+
+        await client.send_message(message.channel, "What is the correct answer?")
+        ans = await client.wait_for_message(author=message.author)
+        ans = ans.content
+
+        dbAlter("INSERT INTO quiz VALUES (?, ?, ?, ?, ?, ?, ?)", None, question, answers[0], answers[1], answers[2], answers[3], ans)
+        await client.send_message(message.channel, 'Question Added')
+    
+    if message.content.startswith('!quizme'):
+        question = dbLookup("SELECT * FROM quiz ORDER BY RANDOM() LIMIT 1")
+        await client.send_message(message.channel, question[0][1])
+        await client.send_message(message.channel, question[0][2])
+        await client.send_message(message.channel, question[0][3])
+        await client.send_message(message.channel, question[0][4])
+        await client.send_message(message.channel, question[0][5])
+        ans = await client.wait_for_message(author=message.author)
+
+        print(question[0][6])
+        print(ans.content)
+        if(ans.content == question[0][6]):
+            await client.send_message(message.channel, "That's correct!")
+        else:
+            await client.send_message(message.channel, "Incorrect. Type !quiz to try again.")
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+client.run(TOKEN)
